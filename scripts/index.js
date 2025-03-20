3268ce115fce36f29bbd1110531866149238d813
moktadir-mirul <moktadir.mirul@gmail.com>
2025-03-20 17:16:34 +0600

    modal added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 const donationArea = document.getElementById('donation-area');
 const historyContainer = document.getElementById('history-container');
+const mainBalance = document.getElementById('main-balance');
+const noakhaliDefault = document.getElementById('noa-default');
+const feniDefault = document.getElementById('feni-default');
+const quotaDefault = document.getElementById('quota-default');
+const noaInput = document.getElementById('noa-input').value;
+const feniInput = document.getElementById('feni-input').value;
+const quotaInput = document.getElementById('quota-input').value;
 let btns = document.querySelectorAll('.active');
+
 function removeActive () {
     for(let i = 0; i< btns.length; i++) { 
         btns[i].classList.remove('active');
@@ -21,3 +29,9 @@ document.getElementById('history-btn').addEventListener('click', function() {
     donationArea.classList.add('hidden');
     historyContainer.classList.remove('hidden');
 })
+
+document.getElementById('noakhali-donate').addEventListener('click', function() {
+    console.log('click hoise');
+    
+}
+)
