34e6092d61f756324d3d4037873ade8884e7d014
moktadir-mirul <moktadir.mirul@gmail.com>
2025-03-22 06:09:13 +0600

    project running

--- scripts/index.js
+++ scripts/index.js
@@ -100,7 +100,38 @@ document.getElementById('feni-donate').addEventListener('click', function(event)
     document.getElementById('feni-input').value = '';
 })
 
+document.getElementById('quota-donate').addEventListener('click', function(event) {
+    event.preventDefault();
+    const quotaInput = Number(document.getElementById('quota-input').value);
+    let totalDonation = Number(quotaDefault.innerText);
+    if(quotaInput && !isNaN(quotaInput)) {
+        if(quotaInput < Number(mainBalance.innerText)) {
+            donateDetails.showModal();
+            let result = quotaInput + totalDonation;
+            quotaDefault.innerHTML= result;
+            let restAmount = Number(mainBalance.innerText) - quotaInput;
+            mainBalance.innerHTML = restAmount;
+            const dateNow = new Date();  
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="w-4/5 mx-auto p-4 border border-gray-200 rounded-lg">
+                <h1 class="text-xl font-bold text-black">
+                    ${quotaInput} Taka is donated for aid for injured in quota movement in Dhaka, Bangladesh
+                </h1>
+                <p class="text-gray-500 text-lg font-light">
+                    Date: ${dateNow}
+                </p>
+            </div>
+            `
+            historyContainer.appendChild(div);
+        }
+        else {
+            alert("You don't have sufficient balance!")
+        }
+    }
+    else {
+        alert('Please input a valid amount to donate')
+    }
+    document.getElementById('quota-input').value = '';
+})
 
-// function preventDefault(e) {
-//     e.preventDefault();
-// }
